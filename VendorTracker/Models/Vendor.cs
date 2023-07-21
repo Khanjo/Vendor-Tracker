c8144556d9c0c3160cc14cbd6375907bbc23b0bd
Joshua Khan <Khanjo@gmail.com>
2023-07-21 13:08:40 -0700

    Pass test for finding Vendor id

--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -21,5 +21,9 @@ namespace VendorTracker.Models
         {
             return _instances;
         }
+        public static Vendor Find(int searchId)
+        {
+            return _instances[searchId - 1];
+        }
     }
 }
