7b5f297c8928fd277ee4f82fda010a0757ee538c
Joshua Khan <Khanjo@gmail.com>
2023-07-21 09:03:52 -0700

    initialize project

---
+++ VendorTracker/Program.cs
@@ -0,0 +1,18 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+WebApplication app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+app.Run();
